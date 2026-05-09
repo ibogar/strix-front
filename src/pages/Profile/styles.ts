b1d34dc574d67e3a8e7786aae7329e8b55eed389
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2026-05-09 11:47:54 -0300

    update Profile page

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Content = styled.div`
     width: 100%;
     display: grid;
+    align-items: start;
+    gap: 16px;
     grid-template-columns: 25% 75%;
     padding: 16px;
 `
