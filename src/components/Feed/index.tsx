5f68a1cdc3b42f7f8fbe6affb0fa45b0e8c99b0f
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2026-05-09 08:15:42 -0300

    add Feed component

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,18 @@
+import * as S from "./styles"
+
+
+const Feed = () => {
+    return (
+        <>
+            <S.FeedContainer>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatem odit perferendis quaerat, eveniet officiis placeat? Veniam, ipsa suscipit est praesentium recusandae, commodi non dolor mollitia aut maxime repellat nostrum?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatem odit perferendis quaerat, eveniet officiis placeat? Veniam, ipsa suscipit est praesentium recusandae, commodi non dolor mollitia aut maxime repellat nostrum?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatem odit perferendis quaerat, eveniet officiis placeat? Veniam, ipsa suscipit est praesentium recusandae, commodi non dolor mollitia aut maxime repellat nostrum?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatem odit perferendis quaerat, eveniet officiis placeat? Veniam, ipsa suscipit est praesentium recusandae, commodi non dolor mollitia aut maxime repellat nostrum?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatem odit perferendis quaerat, eveniet officiis placeat? Veniam, ipsa suscipit est praesentium recusandae, commodi non dolor mollitia aut maxime repellat nostrum?</p>
+            </S.FeedContainer>
+        </>
+    )
+}
+
+export default Feed
---
+++ src/components/Feed/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const FeedContainer = styled.div`
+    width: 100%;
+    padding:16px;
+`
