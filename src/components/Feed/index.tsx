8d19aee7efe1237dd44a2dffe1c14c3f5463d3bc
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2026-05-09 15:57:28 -0300

    update Feed - add page title rule

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,13 +1,21 @@
+import { useLocation } from "react-router-dom"
+import { PageTitle } from "../../styles"
 import NewPost from "../NewPost"
 import Post from "../Post"
 import * as S from "./styles"
 
 
 const Feed = () => {
+    const location = useLocation();
+
     return (
         <>
             <S.FeedContainer>
-                <NewPost />
+                {location.pathname === '/profile' ? (
+                    <PageTitle>Iuri's profile</PageTitle>
+                ): (
+                    <NewPost />
+                )}
                 <Post />
                 <Post />
                 <Post />
